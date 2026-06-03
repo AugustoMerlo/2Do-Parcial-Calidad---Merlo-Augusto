a5f1ab187a6d1e284dc2f0f615f98a54ff79d801
AugustoMerlo <merloaugusto1@gmail.com>
2026-06-03 12:09:20 -0300

    Agrego servidor web para Render

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
+const http = require('http');
+
 function saludar() {
-  return "Hola Mundo";
+  return "Hola Profesores!";
 }
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+  res.end(saludar());
+});
+
+// Render usa la variable PORT automáticamente
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
+
 module.exports = saludar;
